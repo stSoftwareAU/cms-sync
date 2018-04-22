b6c5c903cf0fd0de140bd6ca868f2a1cff9da12c
Nigel Leck <nigel@stsoftware.com.au>
2018-04-22 20:24:06 +1000

    working on dialog

--- src/main/java/com/stsoftware/cms/sync/SyncJFrame.java
+++ src/main/java/com/stsoftware/cms/sync/SyncJFrame.java
@@ -5,6 +5,23 @@
  */
 package com.stsoftware.cms.sync;
 
+import com.aspc.remote.database.InvalidDataException;
+import com.aspc.remote.rest.ReST;
+import com.aspc.remote.rest.Response;
+import com.aspc.remote.rest.Status;
+import com.aspc.remote.util.misc.StringUtilities;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import jdk.net.SocketFlow;
+import org.json.JSONObject;
+
 /**
  *
  * @author nigel
@@ -16,6 +33,33 @@ public class SyncJFrame extends javax.swing.JFrame {
      */
     public SyncJFrame() {
         initComponents();
+        
+        JTextField list[]={
+            password,
+            serverURL,
+            siteName,
+            userName
+        };
+         
+        for( JTextField tf: list)
+        {
+            tf.getDocument().addDocumentListener(new DocumentListener() {
+                @Override
+                public void insertUpdate(DocumentEvent e) {
+                    checkOK();
+                }
+
+                @Override
+                public void removeUpdate(DocumentEvent e) {
+                    checkOK();
+                }
+
+                @Override
+                public void changedUpdate(DocumentEvent e) {
+                    checkOK();
+                }
+            });
+        }
     }
 
     /**
@@ -26,19 +70,25 @@ public class SyncJFrame extends javax.swing.JFrame {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+        bindingGroup = new org.jdesktop.beansbinding.BindingGroup();
 
         okButton = new javax.swing.JButton();
         cancelButton = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+        directoryButton = new javax.swing.JButton();
         directoryPath = new javax.swing.JTextField();
-        remoteURL = new javax.swing.JTextField();
+        serverURL = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         siteName = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        userName = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        password = new javax.swing.JPasswordField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         okButton.setText("OK");
+        okButton.setEnabled(false);
         okButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 okButtonActionPerformed(evt);
@@ -52,86 +102,182 @@ public class SyncJFrame extends javax.swing.JFrame {
             }
         });
 
-        jButton3.setText("Directory");
-
-        directoryPath.setToolTipText("The directory to sync");
-
-        remoteURL.setToolTipText("user:password@server");
-        remoteURL.addActionListener(new java.awt.event.ActionListener() {
+        directoryButton.setText("Directory");
+        directoryButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                remoteURLActionPerformed(evt);
+                directoryButtonActionPerformed(evt);
             }
         });
 
+        directoryPath.setEditable(false);
+        directoryPath.setToolTipText("The directory to sync");
+        directoryPath.setEnabled(false);
+
+        serverURL.setText("https://demo.jobtrack.com.au");
+        serverURL.setToolTipText("server");
+
         jLabel1.setText("Server:");
+
+        org.jdesktop.beansbinding.Binding binding = org.jdesktop.beansbinding.Bindings.createAutoBinding(org.jdesktop.beansbinding.AutoBinding.UpdateStrategy.READ_WRITE, serverURL, org.jdesktop.beansbinding.ObjectProperty.create(), jLabel1, org.jdesktop.beansbinding.BeanProperty.create("labelFor"));
+        bindingGroup.addBinding(binding);
 
         jLabel2.setText("Site:");
 
+        binding = org.jdesktop.beansbinding.Bindings.createAutoBinding(org.jdesktop.beansbinding.AutoBinding.UpdateStrategy.READ_WRITE, siteName, org.jdesktop.beansbinding.ObjectProperty.create(), jLabel2, org.jdesktop.beansbinding.BeanProperty.create("labelFor"));
+        bindingGroup.addBinding(binding);
+
         siteName.setToolTipText("The name of the site to sync");
+
+        jLabel3.setText("User:");
+
+        binding = org.jdesktop.beansbinding.Bindings.createAutoBinding(org.jdesktop.beansbinding.AutoBinding.UpdateStrategy.READ_WRITE, userName, org.jdesktop.beansbinding.ObjectProperty.create(), jLabel3, org.jdesktop.beansbinding.BeanProperty.create("labelFor"));
+        bindingGroup.addBinding(binding);
+
+        userName.setText("admin");
+
+        jLabel4.setLabelFor(password);
+        jLabel4.setText("Password:");
+
+        password.setText("admin");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(directoryButton)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel4))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(directoryPath)
+                    .addComponent(serverURL, javax.swing.GroupLayout.DEFAULT_SIZE, 650, Short.MAX_VALUE)
+                    .addComponent(siteName, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(userName, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(password, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
                 .addGap(42, 42, 42)
                 .addComponent(cancelButton)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(okButton)
                 .addGap(37, 37, 37))
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton3)
-                    .addComponent(jLabel1)
-                    .addComponent(jLabel2))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(remoteURL, javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(directoryPath)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(siteName, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 462, Short.MAX_VALUE)))
-                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton3)
+                    .addComponent(directoryButton)
                     .addComponent(directoryPath, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(remoteURL, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(serverURL, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel1))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
                     .addComponent(siteName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 140, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(userName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(password, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 51, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(okButton)
                     .addComponent(cancelButton))
                 .addContainerGap())
         );
 
+        bindingGroup.bind();
+
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void okButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_okButtonActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_okButtonActionPerformed
+    private void showMessage(final String msg)
+    {
 
-    private void remoteURLActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_remoteURLActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_remoteURLActionPerformed
+        JOptionPane.showMessageDialog(this, msg, "Error", JOptionPane.ERROR_MESSAGE);
+    }
+    
+    private void checkValid( )
+    {
+        try{
+            Response response = ReST.builder(serverURL.getText() + "/ReST/v8/class/Site")
+                    .setParameter("q", "site=" + siteName.getText())
+                    .setAuthorization(userName.getText(), new String(password.getPassword()))
+                    .getResponse();
+            
+            if( response.status == Status.C200_SUCCESS_OK)
+            {
+                JSONObject json = response.getContentAsJSON();
+            }
+            else 
+            {
+                showMessage(response.status.label);
+            }
+        }
+        catch( InvalidDataException | IOException e)
+        {
+            showMessage( e.toString());
+        }
+    }
+    private void okButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_okButtonActionPerformed
+        checkValid();
+    }//GEN-LAST:event_okButtonActionPerformed
 
     private void cancelButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cancelButtonActionPerformed
         // TODO add your handling code here:
         System.exit(1);
     }//GEN-LAST:event_cancelButtonActionPerformed
 
+    private void directoryButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_directoryButtonActionPerformed
+
+        chooser = new JFileChooser();
+        chooser.setCurrentDirectory(new java.io.File("."));
+        chooser.setDialogTitle("Select Directory");
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        //
+        // disable the "All files" option.
+        //
+        chooser.setAcceptAllFileFilterUsed(false);
+        //    
+        if (chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+
+            directoryPath.setText(chooser.getSelectedFile().toString());
+            checkOK();
+        }
+
+    }//GEN-LAST:event_directoryButtonActionPerformed
+
+    private void checkOK()
+    {
+        okButton.setEnabled(false);
+        if( StringUtilities.notBlank(directoryPath.getText())){
+            if( StringUtilities.notBlank(siteName.getText()))
+            {
+                if( StringUtilities.notBlank(userName.getText()))
+                {
+                    if( StringUtilities.notBlank(new String( password.getPassword())))
+                    {
+                        if( StringUtilities.notBlank(serverURL.getText()))
+                        {
+                            okButton.setEnabled(true);
+                        }
+                    }                
+                }                
+            }
+        }
+    }
+    
     /**
      * @param args the command line arguments
      */
@@ -148,33 +294,30 @@ public class SyncJFrame extends javax.swing.JFrame {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(SyncJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(SyncJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(SyncJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(SyncJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new SyncJFrame().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new SyncJFrame().setVisible(true);
         });
     }
 
+    private JFileChooser chooser;
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton cancelButton;
+    private javax.swing.JButton directoryButton;
     private javax.swing.JTextField directoryPath;
-    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JButton okButton;
-    private javax.swing.JTextField remoteURL;
+    private javax.swing.JPasswordField password;
+    private javax.swing.JTextField serverURL;
     private javax.swing.JTextField siteName;
+    private javax.swing.JTextField userName;
+    private org.jdesktop.beansbinding.BindingGroup bindingGroup;
     // End of variables declaration//GEN-END:variables
 }
